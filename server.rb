a407e359eda8652f4fa9b88834a49828bd86ddc1
gitToIgna <mailtoigna@gmail.com>
2016-11-15 17:23:50 +0100

    Add first log in page & break user_create

--- server.rb
+++ server.rb
@@ -2,17 +2,36 @@ require_relative './lib/AsperList.rb'
 require_relative './lib/List.rb'
 require_relative './lib/Item.rb'
 
-user1 = AsperList.new("Igna's List")
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
 
-list1 = List.new("Tom Clancy's books")
-list1.updated_content!("Lista para mostrar los libros que tiene Tom Clancy y poder hacer seguimiento de los que me he leído")
+# user1 = AsperList.new("Igna's List")
+#
+# list1 = List.new("Tom Clancy's books")
+# list1.updated_content!("Lista para mostrar los libros que tiene Tom Clancy y poder hacer seguimiento de los que me he leído")
+#
+# item1 = Item.new("Rainbox Six")
+# item2 = Item.new("Command Autority")
+# item3 = Item.new("Locked On")
+#
+# list1.add_item(item1)
+# list1.add_item(item1)
+# list1.add_item(item1)
+#
+# user1.add_list(list1)
 
-item1 = Item.new("Rainbox Six")
-item2 = Item.new("Command Autority")
-item3 = Item.new("Locked On")
 
-list1.add_item(item1)
-list1.add_item(item1)
-list1.add_item(item1)
+get '/log_in' do
+  erb (:log_in)
+end
 
-user1.add_list(list1)
+post '/create_user' do
+  user = params[:user]
+  if user == ""
+    redirect '/log_in'
+  else
+    user = AsperList.new(user)
+    redirect '/user_profile'
+  end
+end
